91b21447b25f45729cb236f3db154915817759e7
kota oue <kotaoue@gmail.com>
2024-09-04 22:37:06 +0900

    add step

--- helloWorld.kt
+++ helloWorld.kt
@@ -1,8 +1,15 @@
 fun main() {
     val text:String = "Hello World!"
-    println(text)
+    printText(text)
+
+    printCurrentTime()
 }
 
 fun printText(text: String) {
     println(text)
+}
+
+fun printCurrentTime() {
+    println(System.currentTimeMillis())
+    println(java.time.LocalDateTime.now())
 }
